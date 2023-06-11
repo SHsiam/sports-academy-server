4ea80b573262249e5d6005cffe0986de0fa56b18
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-06-12 00:23:41 +0600

    delete items is done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -130,10 +130,17 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/items', async (req, res) => {
+    app.get('/items', verifyJWT, async (req, res) => {
       const result = await itemCollection.find().toArray();
       res.send(result);
     });
+
+    app.delete('/items/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await itemCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
